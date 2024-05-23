d85c0079e3cc4c5ea6d5fb047f1f331561258148
dev-marsu <kevynhdasilva@proton.me>
2024-05-23 20:29:48 -0300

    adding loading animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
-const MenuDiv = document.getElementById('menu-mobile')
-const BtnAnimar = document.getElementById('btn-menu')
-
-MenuDiv.addEventListener('click', animarMenu)
+window.addEventListener('load', function() {
+     document.getElementById('loading').style.display = 'none';
+});
 
 function animarMenu() {
+    const MenuDiv = document.getElementById('menu-mobile')
+    const BtnAnimar = document.getElementById('btn-menu')
+        
+    MenuDiv.addEventListener('click', animarMenu)
     MenuDiv.classList.toggle('abrir')
     BtnAnimar.classList.toggle('ativar')
 }
